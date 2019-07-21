e18fee1f517fabd3ff20f959a316e5a3bd2f4a40
流心 <48278089+sjnode97@users.noreply.github.com>
2019-07-21 16:34:23 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import "./index.scss"
+import Header from "./components/indextop/index"
+class App extends Component {
+  render(){
+        return (
+          <div className="App">
+            <Header>
+
+
+            </Header>
+
+          </div>
+        );
+      }
+}
+export default App;
